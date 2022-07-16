81e90df1cac4a90946d8371f6a1fa188311e221a
kafyar2022 <kafyar2022@gmail.com>
2022-07-16 18:30:39 +0500

    feedback modal desktor

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,43 @@ import { initLocaleList } from '/js/locale-list.js';
 import { initPageNav } from '/js/page-nav.js';
 import { initFooter } from '/js/page-footer.js';
 
+const feedbackButtonEl = document.querySelector('.button--feedback');
+const pageEl = document.querySelector('.page');
+const feedbackModalEl = document.querySelector('.feedback-modal');
+
+function feedbackModalElClickHandler(evt) {
+  if (!evt.target.closest('.feedback-modal__inner')) {
+    pageEl.classList.remove('page--modal-shown');
+    feedbackModalEl.classList.add('feedback-modal--hidden');
+    feedbackModalEl.classList.remove('feedback-modal--shown');
+
+    feedbackModalEl.removeEventListener('click', feedbackModalElClickHandler);
+    document.removeEventListener('keydown', documentKeydownHandler);
+  }
+}
+
+function documentKeydownHandler(evt) {
+  if (evt.keyCode === 27) {
+    pageEl.classList.remove('page--modal-shown');
+    feedbackModalEl.classList.add('feedback-modal--hidden');
+    feedbackModalEl.classList.remove('feedback-modal--shown');
+
+    feedbackModalEl.removeEventListener('click', feedbackModalElClickHandler);
+    document.removeEventListener('keydown', documentKeydownHandler);
+  }
+}
+
+feedbackModalEl.classList.add('feedback-modal--hidden');
+
+feedbackButtonEl.addEventListener('click', () => {
+  pageEl.classList.add('page--modal-shown');
+  feedbackModalEl.classList.remove('feedback-modal--hidden');
+  feedbackModalEl.classList.add('feedback-modal--shown');
+
+  feedbackModalEl.addEventListener('click', feedbackModalElClickHandler);
+  document.addEventListener('keydown', documentKeydownHandler);
+});
+
 initLocaleList();
 initPageNav();
 initFooter();
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,6 +30,12 @@
 
   @include('layouts.footer')
 
+  <section class="feedback-modal">
+    <div class="feedback-modal__inner">
+      <x-feedback-form />
+    </div>
+  </section>
+
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="{{ asset('glide/glide.min.js') }}"></script>
   <script src="{{ asset('js/app.js') }}" type="module"></script>
