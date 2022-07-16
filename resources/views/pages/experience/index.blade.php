99ae37fea7cf3ae036c2be50c8d95bcb3e417227
kafyar2022 <kafyar2022@gmail.com>
2022-07-16 16:39:56 +0500

    experience page desktop 2

--- resources/views/pages/experience/index.blade.php
+++ resources/views/pages/experience/index.blade.php
@@ -6,40 +6,15 @@
 
 @section('content')
   <main class="experience-page">
-    <div class="experience-page__content container" data-content="experience-page-content-{{ $locale }}">
-      <h1>Наш опыт</h1>
-      <p>За годы работы KRESTON АС приобрела успешный опыт сотрудничества с <br>
-        различными компаниями и организациями и на сегодняшний день занимает <br>
-        прочные позиции на рынке, реализуя всевозможные проекты.</p>
-    </div>
+    <div class="experience-page__content container" data-content="experience-page-content-{{ $locale }}">{!! $data['experience-page-content-' . $locale] !!}</div>
     <div class="experience-page__img" style="background-image: url('/files/experience-page-img.jpg')"></div>
 
     <section class="industry-experience container">
-      <div class="industry-experience__top" data-content="industry-experience-top-{{ $locale }}">
-        <h2>Отрослевой опыт</h2>
-        <p>Нашими клиентами являются ведущие компании <br>
-          в следующих отраслях промышленности:</p>
-      </div>
+      <div class="industry-experience__top" data-content="industry-experience-top-{{ $locale }}">{!! $data['industry-experience-top-' . $locale] !!}</div>
 
-      <div class="industry-experience__left" data-content="industry-experience-left-{{ $locale }}">
-        <ul>
-          <li>Промышленное производство</li>
-          <li>Строительство</li>
-          <li>Телекоммуникации</li>
-          <li>Торговля</li>
-          <li>Энергетика</li>
-        </ul>
-      </div>
+      <div class="industry-experience__left" data-content="industry-experience-left-{{ $locale }}">{!! $data['industry-experience-left-' . $locale] !!}</div>
 
-      <div class="industry-experience__right" data-content="industry-experience-right-{{ $locale }}">
-        <ul>
-          <li>Агропромышленный сектор</li>
-          <li>Банковское дело и финансы</li>
-          <li>Авиакомпании</li>
-          <li>Горнодобывающая промышленность, нефть и газ</li>
-          <li>Гостиницы</li>
-        </ul>
-      </div>
+      <div class="industry-experience__right" data-content="industry-experience-right-{{ $locale }}">{!! $data['industry-experience-right-' . $locale] !!}</div>
     </section>
 
     <section class="our-partners container">
