9814c09f2385b90a4617aaa1382ecf36055445cd
kafyar2022 <kafyar2022@gmail.com>
2022-07-29 11:25:19 +0500

    text seeder

--- database/seeders/TextsSeeder.php
+++ database/seeders/TextsSeeder.php
@@ -16,107 +16,108 @@ class TextsSeeder extends Seeder
   {
     $texts = [
       [
-        'slug' => 'company-in-numbers-term-1',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-1-ru',
+        'page' => NULL,
         'text' => '1 971',
       ], [
-        'slug' => 'company-in-numbers-desc-1',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-1-ru',
+        'page' => NULL,
         'text' => 'Год
         основания',
       ], [
-        'slug' => 'company-in-numbers-term-2',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-2-ru',
+        'page' => NULL,
         'text' => '25 000+',
       ], [
-        'slug' => 'company-in-numbers-desc-2',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-2-ru',
+        'page' => NULL,
         'text' => 'Сотрудников
         по всему миру',
       ], [
-        'slug' => 'company-in-numbers-term-3',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-3-ru',
+        'page' => NULL,
         'text' => '700+',
       ], [
-        'slug' => 'company-in-numbers-desc-3',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-3-ru',
+        'page' => NULL,
         'text' => 'Офисов
         обслуживания',
       ], [
-        'slug' => 'company-in-numbers-term-4',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-4-ru',
+        'page' => NULL,
         'text' => '125+',
       ], [
-        'slug' => 'company-in-numbers-desc-4',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-4-ru',
+        'page' => NULL,
         'text' => 'Стран
-        присутствия',
+        присутствия', 'created_at' => '2022-07-29 06:18:08', 'updated_at' => '2022-07-29 06:18:08'
       ], [
-        'slug' => 'company-in-numbers-term-5',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-5-ru',
+        'page' => NULL,
         'text' => '12-я',
       ], [
-        'slug' => 'company-in-numbers-desc-5',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-5-ru',
+        'page' => NULL,
         'text' => 'Крупнейшая глобальная
         бухгалтерская сеть',
       ], [
-        'slug' => 'company-in-numbers-term-6',
-        'page' => null,
+        'slug' => 'company-in-numbers-term-6-ru',
+        'page' => NULL,
         'text' => '$2,3 млрд+',
       ], [
-        'slug' => 'company-in-numbers-desc-6',
-        'page' => null,
+        'slug' => 'company-in-numbers-desc-6-ru',
+        'page' => NULL,
         'text' => 'Доходов
         за 2021 год',
       ], [
-        'slug' => 'details-phone-term',
-        'page' => null,
+        'slug' => 'details-phone-term-ru',
+        'page' => NULL,
         'text' => 'Связь с нами:',
       ], [
-        'slug' => 'details-phone-desc',
-        'page' => null,
+        'slug' => 'details-phone-desc-ru',
+        'page' => NULL,
         'text' => '90 703 23 222',
       ], [
-        'slug' => 'details-email-term',
-        'page' => null,
+        'slug' => 'details-email-term-ru',
+        'page' => NULL,
         'text' => 'Электронная почта:',
       ], [
-        'slug' => 'details-email-desc',
-        'page' => null,
+        'slug' => 'details-email-desc-ru',
+        'page' => NULL,
         'text' => 'info@kreston.tj',
       ], [
-        'slug' => 'details-address-term',
-        'page' => null,
+        'slug' => 'details-address-term-ru',
+        'page' => NULL,
         'text' => 'Таджикистан, Душанбе:',
       ], [
-        'slug' => 'details-address-desc',
-        'page' => null,
+        'slug' => 'details-address-desc-ru',
+        'page' => NULL,
         'text' => 'Улица Пушкина 10, офис 207',
       ], [
-        'slug' => 'details-worktime-term',
-        'page' => null,
+        'slug' => 'details-worktime-term-ru',
+        'page' => NULL,
         'text' => 'Время работы:',
       ], [
-        'slug' => 'details-worktime-desc',
-        'page' => null,
+        'slug' => 'details-worktime-desc-ru',
+        'page' => NULL,
         'text' => 'пн-пт с 09:00 - 18:00',
-      ], [
-        'slug' => '',
-        'page' => '',
-        'text' => '',
       ],
     ];
 
     foreach ($texts as $text) {
-      foreach (config('app.available_locales') as $locale) {
-        Text::create([
-          'locale' => $locale['locale'],
-          'slug' => $text['slug'] . '-' . $locale['locale'],
-          'page' => $text['page'],
-          'text' => $text['text'],
-        ]);
-      }
+      Text::create([
+        'locale' => 'ru',
+        'slug' => $text['slug'] . '-ru',
+        'page' => $text['page'],
+        'text' => $text['text'],
+      ]);
+
+      Text::create([
+        'locale' => 'en',
+        'slug' => substr($text['slug'], 0, -2) . 'en',
+        'page' => $text['page'],
+        'text' => $text['text'],
+      ]);
     }
   }
 }
